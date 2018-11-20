f99b7f2b8cc10833c6c792b47c7ab1d03c07633e
Chandrakant <cadaats@gmail.com>
2018-11-19 21:56:10 -0500

    added flexbox component and default routed to this component

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
-
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material';
 import { FlexboxComponent } from './flexbox/flexbox.component';
 @NgModule({
@@ -12,7 +13,9 @@ import { FlexboxComponent } from './flexbox/flexbox.component';
   imports: [
     CommonModule,
     DemoRoutingModule,
+    FormsModule,
     MaterialModule,
+    FlexLayoutModule,
     MatIconModule
   ]
 })
